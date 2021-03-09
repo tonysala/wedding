2da90496f65c5ce4972cae4bcf3e49cc57b5a968
Tony Sala <tonysala32@gmail.com>
2021-03-09 13:14:29 +0000

    Added controller to RSVP

---
+++ app/Http/Controllers/RsvpController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class RsvpController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required',
+            'number' => 'required|string',
+            'attend' => 'required|string',
+            'notes' => 'string',
+        ]);
+
+        Log::info(json_encode($fields));
+
+        return response()->json([
+            'error' => false,
+        ]);
+    }
+}
